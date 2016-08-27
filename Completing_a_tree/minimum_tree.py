3e94001e554035dae56184aa15c666501bf52951
serbanc94 <serbanc94@gmail.com>
2016-08-27 15:28:40 +0300

    up

--- Completing_a_tree/minimum_tree.py
+++ Completing_a_tree/minimum_tree.py
@@ -1,19 +1,26 @@
 import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree
 
 f=open("sample.txt","r")
 no=int(f.readline())
 
-adj_mat=np.eye(no,dtype=int)
-
+adj_mat=np.full((no,no),1)
+num=0
 for pair in f.readlines():
     a,b= int(pair.split(" ")[0]), int(pair.split(" ")[1].strip("\n"))
-    adj_mat[a-1][b-1]=adj_mat[b-1][a-1]=1
-num=0
-for i in range(no):
-    for j in range(no):
-        if (adj_mat[i][j]==adj_mat[j][i]==1 and i!=j):
-            num+=1
-print no-num/2
+    adj_mat[a-1][b-1]=2
+    num+=1
 
 
+print adj_mat
+X=csr_matrix(adj_mat)
 
+
+Tcsr=minimum_spanning_tree(X)
+#Tcsr.toarray().astype(int)
+i=0
+for line in Tcsr:
+    i+=1
+
+print i-num-1
